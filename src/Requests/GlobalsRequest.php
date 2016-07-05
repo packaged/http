46bbdc6cfb4a374f1877d8b41689a6125ba48d24
Brooke Bryan <brooke@bajb.net>
2016-07-05 17:01:48 +0100

    Server Request

--- src/Requests/GlobalsRequest.php
+++ src/Requests/GlobalsRequest.php
@@ -2,24 +2,34 @@
 namespace Packaged\Http\Requests;
 
 use Packaged\Http\Helpers\ArrayHelper;
-use Packaged\Http\Request;
+use Packaged\Http\ServerRequest;
 use Packaged\Http\Streams\PhpInputStream;
 use Packaged\Http\Uris\GlobalsUri;
 
-class GlobalsRequest extends Request
+class GlobalsRequest extends ServerRequest
 {
   public function __construct()
   {
     $this->body = new PhpInputStream();
     $this->method = ArrayHelper::value($_SERVER, 'REQUEST_METHOD', 'GET');
-    $this->protocol = stristr(
-      ArrayHelper::value($_SERVER, 'SERVER_PROTOCOL'),
-      '1.1'
-    ) ? '1.1' : '1.0';
+    $protocol = ArrayHelper::value($_SERVER, 'SERVER_PROTOCOL');
+    $this->protocol = stristr($protocol, '1.1') ? '1.1' : '1.0';
 
     foreach(getallheaders() as $header => $value)
     {
       $this->addHeader($header, $value);
+    }
+
+    $this->query = $_GET;
+    $this->cookies = $_COOKIE;
+
+    if(!empty($_POST))
+    {
+      $this->parsedBody = $_POST;
+    }
+    else
+    {
+      $this->parsedBody = $this->getBody();
     }
 
     $this->uri = new GlobalsUri();
